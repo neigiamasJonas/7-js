4f114d923722a1738e5b5d9a1aa0ae33146be6b6
neigiamasJonas <p.ezerskis@gmail.com>
2022-03-30 13:11:11 +0300

    pamoka

---
+++ js/01-nd.js
@@ -0,0 +1,60 @@
+// UZDUOTYS //
+
+// Kintamuju inicijavimas //
+
+const pirmasSkaicius = 2;
+console.log('Pirmas skaicius', pirmasSkaicius);
+
+const antrasSkaicius = 4;
+console.log('Antras skaicius', antrasSkaicius);
+
+const treciasSkaicius = 7;
+console.log('Trecias skaicius', treciasSkaicius);
+
+
+const pirmasZodis = 'Kas';
+console.log('Pirmas zodis', pirmasZodis);
+
+const antrasZodis = 'Ravi';
+console.log('Antras zodis', antrasZodis);
+
+const treciasZodis = 'Siena';
+console.log('Trecias zodis', treciasZodis);
+
+
+const pirmoSarNr = [1,3,5,7,10];
+console.log('Pirmo saraso numeriai', pirmoSarNr);
+pirmoSarNr[0]="1";
+pirmoSarNr[1]="3";
+pirmoSarNr[2]="5";
+pirmoSarNr[3]="7";
+pirmoSarNr[4]="9";
+
+
+const antroSarNr = [2,4,6,8,9];
+console.log('Antro saraso numeriai', antroSarNr);
+
+const trecioSarNr = [12,34,56,78,910];
+console.log('Trecio saraso numeriai', trecioSarNr);
+
+
+const pirmoSarZodziai = ['Kas','turi','paskolinti','geltona','flomasteri'];
+console.log('Pirmo saraso zodiai', pirmoSarZodziai);
+
+const antroSarZodziai = ['Kodel','tu','klausi','jeigu','zinai'];
+console.log('Antro saraso zodiai', antroSarZodziai);
+
+const trecioSarZodziai = ['Kiek','tau','kartu','reiks','kartoti'];
+console.log('Trecio saraso zodiai', trecioSarZodziai);
+
+// Veiksmai su kintamaisiais //
+
+// 1 //
+const visuSkaiciuSuma = 'suma'
+console.log('Visu skaiciu suma =', pirmasSkaicius + antrasSkaicius + treciasSkaicius);
+
+// 2 //
+const zodziaiSuTarpais = 'zodiai su tarpais'
+console.log('Zodiai su tarpais -', pirmasZodis, antrasZodis, treciasZodis);
+
+// 3 //
---
+++ js/03-strings.js
@@ -0,0 +1,49 @@
+/*
+STRING
+kabutes:
+- viengubos (')
+- dvygubos (")
+- backtick'as (`)
+Teksto simboliu kiekis: .length
+*/
+
+const name = 'Petras';
+console.log(name);
+
+const lastname = "Petraitis";
+console.log(lastname);
+
+const viegubaKabute = "viengubos (')";
+console.log(viegubaKabute);
+
+const dvigubaKabute = 'dvigubos (")';
+console.log(dvigubaKabute);
+
+const viegubaKabute12 = 'Cia yra vienguba (\') ir dviguba (") kabutes.';
+console.log(viegubaKabute12);
+
+const dvigubaKabute12 = "Cia yra vienguba (') ir dviguba (\") kabutes.";
+console.log(dvigubaKabute12);
+
+const kabute12 = "Cia yra vienguba (\') ir dviguba (\") kabutes.";
+console.log(kabute12);
+
+const clientName = 'Maryte';
+const clientAge = 87;
+// Client Maryte is 87 years old.
+const clientSentence = 'Client ' + clientName + ' is ' + clientAge + ' years old.';
+console.log(clientSentence);
+
+const backtickClient = `Client ${clientName} is ${clientAge} years old.`;
+console.log(backtickClient);
+
+const n1 = 7;
+const n2 = 5;
+const rez = n1 + n2;
+// 7 + 5 = 12
+const ats = `${n1} + ${n2} = ${rez}`;
+console.log(ats);
+
+const abc = 'ab"cd\'e\\\\f';
+const abcDydis = abc.length;
+console.log(`Abeceles "${abc}" dydis yra ${abcDydis} simboliu.`);
